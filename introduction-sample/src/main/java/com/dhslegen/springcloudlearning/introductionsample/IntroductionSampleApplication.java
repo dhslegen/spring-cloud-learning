950d46cb14793ca5ea4306217d8395ac7de44b9f
ZhaoWH <dhslegen@163.com>
2019-11-30 23:34:53 +0800

    init

---
+++ introduction-sample/src/main/java/com/dhslegen/springcloudlearning/introductionsample/IntroductionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.dhslegen.springcloudlearning.introductionsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionSampleApplication.class, args);
+    }
+
+}
---
+++ introduction-sample/src/test/java/com/dhslegen/springcloudlearning/introductionsample/IntroductionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhslegen.springcloudlearning.introductionsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
