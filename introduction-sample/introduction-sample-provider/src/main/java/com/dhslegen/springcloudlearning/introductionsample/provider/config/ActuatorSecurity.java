06fe2794fa26edc158b186aa5f027250c13688e5
zhaowenhao <dhslegen@163.com>
2020-07-08 23:19:35 +0800

    init

---
+++ introduction-sample/introduction-sample-provider/src/main/java/com/dhslegen/springcloudlearning/introductionsample/provider/config/ActuatorSecurity.java
@@ -0,0 +1,21 @@
+package com.dhslegen.springcloudlearning.introductionsample.provider.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhaowenhao
+ */
+@Configuration(proxyBeanMethods = false)
+public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests((requests) ->
+                requests.anyRequest().permitAll());
+        http.httpBasic();
+    }
+
+}
