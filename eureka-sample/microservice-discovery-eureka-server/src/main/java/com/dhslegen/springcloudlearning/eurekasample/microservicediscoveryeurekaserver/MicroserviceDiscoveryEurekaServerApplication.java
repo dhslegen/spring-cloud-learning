5f98164d53a7f0daaede34211fab59b8fdf2753c
zhaowenhao <dhslegen@163.com>
2020-07-15 21:06:39 +0800

    init11

---
+++ eureka-sample/microservice-discovery-eureka-server/src/main/java/com/dhslegen/springcloudlearning/eurekasample/microservicediscoveryeurekaserver/MicroserviceDiscoveryEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.dhslegen.springcloudlearning.eurekasample.microservicediscoveryeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDiscoveryEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDiscoveryEurekaServerApplication.class, args);
+    }
+
+}
---
+++ eureka-sample/microservice-discovery-eureka-server/src/test/java/com/dhslegen/springcloudlearning/eurekasample/microservicediscoveryeurekaserver/MicroserviceDiscoveryEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhslegen.springcloudlearning.eurekasample.microservicediscoveryeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
